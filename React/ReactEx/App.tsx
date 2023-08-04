69fb7c65fa0eba4749170a3a9feb1bc45c15ef7b
Giovi666 <giovannadipaolo39@gmail.com>
2023-08-04 13:42:07 -0300

    Delete some Exercises

---
+++ React/ReactEx/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import StateApp from 'React/ReactEx/State/State.tsx';
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <StateApp />
+    </div>
+  );
+}
+
+export default App;
--- ReactExercises/react-project/src/Context/Components/LanguageContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export enum Language {
-  English = 'english',
-  Spanish = 'spanish',
-}
-
-export const LanguageContext = React.createContext<Language>(Language.English);
